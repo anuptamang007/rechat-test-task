72fed850ef77715390792d8076ff06d6f28f3d9f
anuptamang007 <anuptamang007@gmail.com>
2023-04-26 13:25:13 +0545

    feat(api): project api is updated to use localstorage as db

--- src/context/api/posts.ts
+++ src/context/api/posts.ts
@@ -1,69 +1,63 @@
-import { Task } from '../task/types';
+import { loadState } from 'src/utilities';
 
-export const getAllTaskApi = async () => {
-  const tasks = localStorage.getItem('tasks');
-  return tasks ? JSON.parse(tasks) : [];
+import { initialState } from '../task';
+import { Task, TaskHistory } from '../task/types';
+
+export const getAllTaskApi = () => {
+  const tasks = loadState('tasks') || initialState;
+  return tasks.data as Task[];
 };
 
-export const getTaskByIdApi = async (id: string) => {
-  const tasks = localStorage.getItem('tasks');
-  return tasks
-    ? (JSON.parse(tasks).find((task: Task) => task.id === id) as Task)
-    : ({} as Task);
+export const getTaskByIdApi = (id: string) => {
+  const tasks = loadState('tasks') || initialState;
+  const task = tasks.data.find((item: Task) => item.id === id);
+
+  return task as Task;
 };
 
-export const createTaskApi = async (task: Task) => {
-  const tasks = localStorage.getItem('tasks') || '[]';
+export const createTaskApi = (task: Task) => {
   const data = {
     title: task.title,
     description: task.description,
     status: task.status,
-  };
-  const newTask = {
     id: Math.random().toString(36).substr(2, 9),
     createdAt: new Date().getTime(),
+  };
+  const newTask: Task = {
     ...data,
+    lastUpdated: data.createdAt,
     history: [
       {
         id: Math.random().toString(36).substr(2, 9),
         task: data,
         updatedAt: new Date().getTime(),
       },
-    ],
+    ] as TaskHistory[],
   };
-  const newTasks = [...JSON.parse(tasks), newTask];
-  localStorage.setItem('tasks', JSON.stringify(newTasks));
   return newTask as Task;
 };
 
-export const editTaskByIdApi = async (task: Task) => {
-  const tasks = localStorage.getItem('tasks') || '[]';
+export const editTaskByIdApi = (task: Task) => {
+  const data = {
+    title: task.title,
+    description: task.description,
+    status: task.status,
+    id: task.id,
+    createdAt: task.createdAt,
+    lastUpdated: new Date().getTime(),
+  };
 
-  const newTasks = JSON.parse(tasks).map((item: Task) => {
-    if (item.id === task.id) {
-      const history = [
-        { ...item.history },
-        {
-          id: Math.random().toString(36).substr(2, 9),
-          task: {
-            title: task.title,
-            description: task.description,
-            status: task.status,
-          },
-          updatedAt: new Date().getTime(),
-        },
-      ];
-      return {
-        ...item,
-        title: task.title,
-        description: task.description,
-        status: task.status,
-        history,
-      };
-    }
-    return item;
-  });
+  const newTask: Task = {
+    ...data,
+    history: [
+      ...task.history,
+      {
+        id: Math.random().toString(36).substr(2, 9),
+        updatedAt: new Date().getTime(),
+        task: data,
+      },
+    ] as TaskHistory[],
+  };
 
-  localStorage.setItem('tasks', JSON.stringify(newTasks));
-  return task as Task;
+  return newTask as Task;
 };
